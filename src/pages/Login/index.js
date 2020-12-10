2808aa35138e9a9ad0b005e0c9d6560a78644211
Chou Cheng Lei <rex@eatme.tw>
2020-12-10 18:10:09 +0800

    Adjust login section layout

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -11,6 +11,7 @@ import { useHistory, useLocation } from "react-router-dom"
 import axios from "axios"
 import logoIcon from "../../images/logo.svg"
 import queryString from "query-string"
+import "./styles/style.scss"
 
 export default (props) => {
   const handlelogin = () => {
@@ -24,25 +25,52 @@ export default (props) => {
       <div className="login__section">
         <div className="login__section__content">
           <h1 className="title is-1 has-text-white">
-            <div className="columns is-gapless">
-              <div className="column is-2">
-                <span className="icon is-large" style={{ width: 120 }}>
-                  <img src={logoIcon} alt="" />
-                </span>
-              </div>
-              <div className="column is-10" style={{ margin: "auto" }}>
-                Spotify Connect
-                <p className="title is-5 has-text-white mt-2 ml-1">
-                  Continue to play and hear half of the music seamlessly.
+            <div className="columns is-variable is-5">
+              <div className="column is-4 has-text-centered">
+                <p class="mb-5 columns is-variable is-0">
+                  <div class="column" style={{ marginTop: 9.5 }}>
+                    <img
+                      width="200"
+                      src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_RGB_White.png"
+                      alt=""
+                    />
+                  </div>
+                  <div class="column has-text-left">
+                    <span class="title is-3 has-text-white">Connect</span>
+                  </div>
                 </p>
-                <div className="buttons">
-                  <button
-                    className="button is-rounded is-outlined has-text-weight-bold"
-                    onClick={handlelogin}
-                  >
-                    LOGIN
-                  </button>
+
+                <div class="columns is-mobile is-multiline">
+                  <div class="column is-12">
+                    <button
+                      class="button  has-text-white has-text-weight-bold  is-rounded is-fullwidth p-5"
+                      onClick={handlelogin}
+                      style={{ background: "#3D83FF", border: 0 }}
+                    >
+                      SIGN UP
+                    </button>
+                  </div>
+                  <div class="column is-12">
+                    <button
+                      class="button is-light is-rounded has-text-weight-bold is-fullwidth p-5"
+                      onClick={handlelogin}
+                    >
+                      LOGIN
+                    </button>
+                  </div>
                 </div>
+              </div>
+              <div
+                className="column is-8 has-text-centered"
+                style={{ margin: "auto" }}
+              >
+                <p class="has-text-left title is-1 has-text-white">
+                  Get the right music, righ now
+                </p>
+
+                <p className="subtitle is-5 has-text-white has-text-weight-light mt-2 ml-1 has-text-left">
+                  Listen to millions of songs for free
+                </p>
               </div>
             </div>
           </h1>
